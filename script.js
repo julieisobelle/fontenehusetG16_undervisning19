c553e8869089f57029ae7a718951e3b1a8636e1f
julieisobelle <git@stackblitz.com>
2026-01-20 10:13:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Basic Array/Flat Array
+const names = ["Julie", "Fredrik", "Stefan", 10, 20, 30, true, false];
+console.log(names);
+
+// LA STÅ
+const displayHere = document.querySelector("#displayHere");
+console.log(displayHere);
+
+displayHere.textContent = undefined;
